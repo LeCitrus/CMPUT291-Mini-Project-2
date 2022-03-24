240453ec0f17254eadbc91a70037d7edced82098
LeCitrus <68800077+LeCitrus@users.noreply.github.com>
2022-03-23 21:17:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+# Connect to 291db.db
+# Should be able to run the 5 tasks
+
 from pymongo import MongoClient
 import json
+
+while True:
+   try:
+        port = int(input("Enter port number: "))
+        break
+   except ValueError:
+        print("Invalid input!")
