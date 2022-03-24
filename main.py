615aa289d19daedf846bb24d01bf6e5072553a73
LeCitrus <68800077+LeCitrus@users.noreply.github.com>
2022-03-23 22:56:39 -0600

    simple query interface

--- main.py
+++ main.py
@@ -1,21 +1,122 @@
-# Connect to 291db.db
-# Should be able to run the 5 tasks
-
 from pymongo import MongoClient
 import json
+import os
+
+# For clearing terminal
+if os.name == 'nt':
+    clr = 'cls'
+else:
+    clr = 'clear'
+
 
 # Get port number
 def get_port():
-   while True:
-      try:
-           port = int(input("Enter port number: "))
-           return port
-      except ValueError:
-           print("Invalid input!")
+    while True:
+        try:
+            port = int(input("Enter port number: "))
+            return port
+        except ValueError:
+            print("Invalid input!")
 
-            
+
+# Main menu prompt
+def print_main_menu():
+    os.system(clr)
+    print("List of tasks\n------------------------------")
+    print("1. Search for titles\n2. Search for genres\n3. Search for cast/crew members\n4. Add a movie\n5. Add a "
+          "cast/crew member\n6. Exit\n")
+
+
+# Get task number
+def get_task():
+    while True:
+        try:
+            inp = int(input("Execute task: "))
+            if inp in (1, 2, 3, 4, 5, 6):
+                return inp
+            else:
+                print("Please enter a valid task number! (1, 2, 3, 4, 5, 6)")
+        except ValueError:
+            print("Invalid input!")
+
+
+# Search for titles
+def task_1():
+    os.system(clr)
+    keywords = list(input("Enter 1 or more keywords, separated by spaces (eg. cmput MaTh DAVOOD): ").split())
+    print('pog')
+
+
+# Search for genres
+def task_2():
+    os.system(clr)
+    genre = input("Enter genre: ")
+    while True:
+        try:
+            count = int(input("Enter minimum vote count: "))
+            break
+        except ValueError:
+            print("Please enter an integer.")
+
+
+# Search for cast/crew members
+def task_3():
+    os.system(clr)
+    name = input("Enter cast/crew member name: ")
+
+
+# Add a movie
+def task_4():
+    os.system(clr)
+    mid = input("Enter unique MID: ")
+    title = input("Enter title: ")
+    while True:
+        try:
+            year = int(input("Enter start year: "))
+            break
+        except ValueError:
+            print("Invalid input!")
+    while True:
+        try:
+            running_time = int(input("Enter running time: "))
+            break
+        except ValueError:
+            print("Invalid input!")
+    genres = list(input("Enter genre(s), separated by spaces (eg. action cOmEdy HORROR): ").split())
+
+
+# Add a cast/crew member
+def task_5():
+    os.system(clr)
+    cid = input("Enter CID: ")
+    title = input("Enter title id: ")
+    category = input("Enter category: ")
+
+
+# Main program
 def main():
-   port = get_port()
-   
+    port = get_port()
+    while True:
+        print_main_menu()
+        task = get_task()
+        if task == 6:
+            quit("Goodbye!")
+        elif task == 1:
+            task_1()
+            input("\n*Enter key to go back*")
+        elif task == 2:
+            task_2()
+            input("\n*Enter key to go back*")
+        elif task == 3:
+            task_3()
+            input("\n*Enter key to go back*")
+        elif task == 4:
+            task_4()
+            input("\n*Enter key to go back*")
+        elif task == 5:
+            task_5()
+            input("\n*Enter key to go back*")
+
+
 if __name__ == "__main__":
     main()
