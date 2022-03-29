beb2759e5c5a3b02e6fcfaaae230012d7b89c6e3
Curtis Kan <curtis.tl.kan@gmail.com>
2022-03-29 17:59:00 -0600

    tsv json nice

--- tsv-2-json.py
+++ tsv-2-json.py
@@ -1,21 +1,19 @@
 
 import json
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def tsv2json(input_file, output_file):
-
-
     arr = [] 
     file = open(input_file, 'r') 
     title_line = file.readline()
 
-    # The first line consist of headings of the record 
-    # so we will store it in an array and move to 
-    # next line in input_file.
+    # The first line consist of headings of the record, so we will store it in an array and move to next line in input_file.
     titles = [title.strip() for title in title_line.split('\t')]
+
     for line in file:
         row = {}
+
         for key, value in zip(titles, line.split('\t')):
 
             # Convert each row into dictionary with keys as titles
@@ -25,32 +23,34 @@ def tsv2json(input_file, output_file):
             if key in ("primaryProfession", "knownForTitles", "genres", "characters"):
                 row[key] = value.strip().split(',')
 
-
         # We will use strip to remove '\n'.n
         arr.append(row)
 
-        # We will append all the individual dictionaries into list and dump into file
+    # We will append all the individual dictionaries into list and dump into file
     with open(output_file, 'w', encoding='utf-8') as output_file:
         output_file.write(json.dumps(arr, indent=4))
 
 
 # Driver Code
-#location = input('Please enter the directory of the tsv and json files: ')
+def main():
+    # Get current directory
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-files_in = [dir_path + '/name.basics.tsv', 
-dir_path + '/title.basics.tsv',
-dir_path + '/title.principals.tsv',
-dir_path + '/title.ratings.tsv']
+    # Prepare filenames for input and output
+    files_in = [dir_path + '/name.basics.tsv', 
+    dir_path + '/title.basics.tsv',
+    dir_path + '/title.principals.tsv',
+    dir_path + '/title.ratings.tsv']
 
-files_out = [dir_path+'/name.basics.json', 
-dir_path + '/title.basics.json',
-dir_path + '/title.principals.json',
-dir_path + '/title.ratings.json']
+    files_out = [dir_path + '/name.basics.json', 
+    dir_path + '/title.basics.json',
+    dir_path + '/title.principals.json',
+    dir_path + '/title.ratings.json']
 
-for i in range(0,4):
-    input_filename = files_in[i]
-    output_filename = files_out[i]
-    tsv2json(input_filename,output_filename)
+    for i in range(4):
+        input_filename = files_in[i]
+        output_filename = files_out[i]
+        tsv2json(input_filename, output_filename)
 
-
-
+if __name__ == "__main__":
+    main()
