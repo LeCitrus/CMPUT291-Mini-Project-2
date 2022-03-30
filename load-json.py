c00aeaa32c37c23d58f39b32c138b094fc20267c
Clare Chen <77589760+claremaodou@users.noreply.github.com>
2022-03-29 23:50:49 -0600

    Update load-json.py

--- load-json.py
+++ load-json.py
@@ -1,11 +1,12 @@
 # Make MongoDB collection from the 4 .jsons
 # Port number input, connect to server, create 291db.db
-import json
 from pymongo import MongoClient 
+import json
 import os
 
+# Creating File path
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
+   
 while True:
    try:
         port = input("Enter port number: ")
@@ -14,10 +15,16 @@ while True:
         print("Invalid input!")
 
 # Making Connection
-myclient = MongoClient("mongodb://localhost:"+port+'/') 
-   
-def main():  
+def connector(port):
+    # connects to db
+    if port == None:
+        return -1
+    else:
+        myclient = MongoClient("mongodb://localhost:"+port+'/') 
+        return myclient
 
+
+def main(myclient):  
     # database 
     db = myclient["291db"]   
     # Created or Switched to collection 
@@ -26,32 +33,50 @@ def main():
     title_basics = db["title_basics"]
     title_principals = db["title_principals"]
     title_ratings = db["title_ratings"]
+    
+    clear_collections(name_basics, title_basics, title_principals, title_ratings)
 
-    name_basics.drop()
-    title_basics.drop()
-    title_principals.drop()
-    title_ratings.drop()
-
-    # Loading or Opening the json file
+    # Loading / Opening the json file
     with open(dir_path+'/name.basics.json') as file:
         file_data = json.load(file)
-    name_basics.insert_many(file_data)  
+    name_basics.insert_many(file_data) 
+    print("file opened") 
     file.close()
 
     with open(dir_path+'/title.basics.json') as file:
         file_data = json.load(file)
-    title_basics.insert_many(file_data)  
+    title_basics.insert_many(file_data) 
+    print("file opened") 
     file.close()
 
     with open(dir_path+'/title.principals.json') as file:
         file_data = json.load(file)
     title_principals.insert_many(file_data)  
+    print("file opened")
     file.close()
 
     with open(dir_path+'/title.ratings.json') as file:
         file_data = json.load(file)
     title_ratings.insert_many(file_data)  
+    print("file opened")
     file.close()
 
+    # create indexing
+    db['title_basics'].create_index('tconst')
+    db['title_ratings'].create_index('tconst')
+    db['name_basics'].create_index('nconst')
+    db['title_principals'].create_index('nconst')
+    db['title_principals'].create_index('tconst')
+
+def clear_collections(name_basics, title_basics, title_principals, title_ratings):
+    # drops all collections
+    # for demo
+    name_basics.drop()
+    title_basics.drop()
+    title_principals.drop()
+    title_ratings.drop()
+
 if __name__ == "__main__":
-    main()
+    myclient = connector(port)
+    main(myclient)
+ 
